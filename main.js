027808d156eef52ad21d620f350eb350ed87ab0d
jloh02 <36648707+jloh02@users.noreply.github.com>
2020-05-15 18:08:38 +0800

    Basic youtube code

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
-const bot = new Discord.Client();
+const config = require('./config.json');
+const client = new Discord.Client();
 
-bot.once('ready', () => {
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
 	console.log('Bot started');
 });
 
-bot.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+client.on('message', message => {
+	if (!message.content.startsWith(config.bot.prefix) || message.author.bot) return;
+	
+	const args = message.content.slice(config.bot.prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
-	const args = message.content.slice(prefix.length).split(/ +/);
 
-	if (command === `ping`) message.channel.send('pong!');
-	else if (command === `beep`) message.channel.send('boop!');
+	if (!client.commands.has(command)) return;
 
-	//Insert module function for message checks here: fn(message)
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
 });
 
-bot.login(token).then(()=>bot.user.setActivity(`${prefix}help`, { type: 'PLAYING' }));
+client.login(config.bot.token).then(()=>client.user.setActivity(`${config.bot.prefix}help`, { type: 'PLAYING' }));
